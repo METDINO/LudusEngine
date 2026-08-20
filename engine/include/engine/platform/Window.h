9ad820bc5cf0aa353e5adbc9d65f66a8150f3f3c
METDINO <chad.c.berry@gmail.com>
2026-08-20 15:13:59 -0400

    Added Color

--- engine/include/engine/platform/Window.h
+++ engine/include/engine/platform/Window.h
@@ -52,6 +52,9 @@ public:
 private:
     SDL_Window*   m_window   = nullptr;
     SDL_Renderer* m_renderer = nullptr;
+
+    bool sdl_videoInitialized = false;
+
 };
 
 } // namespace eng
--- sandbox/src/main.cpp
+++ sandbox/src/main.cpp
@@ -19,6 +19,16 @@ int main(int argc, char** argv) {
     // TODO(week1): Initialize a SDL3 Window. Check if it IsValid(). Bail with a message
     // and a non-zero exit code if it failed.
 
+    eng::Window window("Start!", 1280, 720);
+
+    if (!window.IsValid())
+    {
+        std::print(stderr, "Failed to create window\n");
+        return 1;
+    }
+
+    
+
     bool running = true;
     while (running) {
         // TODO(week1): drain the SDL event queue with SDL_PollEvent.
@@ -30,7 +40,28 @@ int main(int argc, char** argv) {
         // lag.
 
         // TODO(week1): clear to a colour of your choosing, then present.
+    
+        SDL_Event event;
+        while (SDL_PollEvent(&event))
+        {
+            switch (event.type)
+            {
+                case SDL_EVENT_QUIT:
+                case SDL_EVENT_WINDOW_CLOSE_REQUESTED:
+                    running = false;
+                    break;
+                default:
+                    //ignore other events // Do nothing
+                    break;
+            }
+        }
+
+        window.Clear(69, 420, 666);
+
+        window.Present();
+    
     }
+
 
     std::printf("Clean exit.\n");
     return 0;
